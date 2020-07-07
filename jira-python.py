ef47ac613b441cf9824951026a510b3e37386dfc
Pablo <pablonieto02@gmail.com>
2020-07-07 11:51:50 -0300

    a

--- jira-python.py
+++ jira-python.py
@@ -1,3 +1,14 @@
 from jira import JIRA
+import jiraToken as token
 
-jira = JIRA()
+jira = JIRA('https://jira.atlassian.com')
+
+api_token = token.getToken()
+email = token.getEmail()
+
+auth_jira = JIRA(auth=(email, api_token))
+issue = jira.issue('TPO-3')
+
+print(issue.fields.project.key)             # 'JRA'
+print(issue.fields.issuetype.name)           # 'New Feature'
+print(issue.fields.reporter.displayName)    # 'Mike Cannon-Brookes [Atlassian]'
